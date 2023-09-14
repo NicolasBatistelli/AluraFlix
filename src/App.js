d34ebdc88ef42bcf260f6a4cb1eb5cd421fa6966
NicolasBatistelli <nicolasbati8@gmail.com>
2023-09-13 23:28:48 -0300

    feat:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
+import Home from "./pages/Home"
+import NewCategory from "./pages/NewCategory"
+import NewVideo from "./pages/NewVideo"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Page404 from './pages/Page404';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <GlobalStyle />
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/NewVideo' element={<NewVideo />}/>
+        <Route path='/newcategory' element={<NewCategory />}/>
+        <Route path='*' element={<Page404 />}/>
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
